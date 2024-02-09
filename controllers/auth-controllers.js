34b56596790164de15bd79b67f09955eae3b0fc0
IvaZach <ivazacherniuk@gmail.com>
2024-02-09 18:19:34 +0300

    add signup

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,22 +1,49 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import { HttpError } from "../helpers/index.js";
-
 import { ctrlWrapper } from "../decorators/index.js";
 
-const singup = async (req, res) => {
-    const {email} = req.body;
-    const user = await User.findOne({email});
-    if(user) {
-        throw HttpError(409, "Email already in use");
-    }
+const {JWT_SECRET} = process.env
 
-    const newUser = await User.create(req.body);
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
 
-    res.status(201).json({
-        email: newUser.email
-    })
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    email: newUser.email,
+  });
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+const payload = {
+    id: user.id
+}
+  const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "23h"});
+  await User.findByIdAndUpdate(user.id, { token })
+
+  res.json({
+    token,
+  });
 };
 
 export default {
-  singup: ctrlWrapper(singup),
+  signup: ctrlWrapper(signup),
+  signin: ctrlWrapper(signin),
 };

b69f4911a77d7cbc6d7578cd2ed0fdccd95dfefd
IvaZach <ivazacherniuk@gmail.com>
2024-02-15 16:19:22 +0300

    add test

---
+++ routes/api/auth-router.test.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import app from "../../app.js";
+import User from "../../models/User.js";
+
+const { DB_TEST_HOST, PORT = 3000 } = process.env;
+
+describe("test /api/users/login route", () => {
+  let server = null;
+  const registerData = {
+    email: "anna@k.com",
+    password: "123456",
+    subscription: "pro",
+  };
+  beforeAll(async () => {
+    await mongoose.connect(DB_TEST_HOST);
+    server = app.listen(PORT);
+    await request(app).post("/api/users/register").send(registerData);
+  });
+
+  afterAll(async () => {
+    await User.deleteMany();
+    await mongoose.connection.close();
+    server.close();
+  });
+
+  test("test /api/users/login correctData", async () => {
+    const loginData = {
+      email: "anna@k.com",
+      password: "123456",
+    };
+
+    const { body, statusCode } = await request(app)
+      .post("/api/users/login")
+      .send(loginData);
+
+    const userData = await User.findOne({ email: loginData.email });
+
+    expect(statusCode).toBe(200);
+    expect(loginData.email).toBe(body.user.email);
+    expect(userData.email).toBe(loginData.email);
+    expect(userData.token).toBe(body.token);
+    expect(userData.subscription).toBe(body.user.subscription);
+    expect(userData.email).toBe(body.user.email);
+    expect(body.user).toBe(objectContaining({email,subscription}))
+  
+  });
+});

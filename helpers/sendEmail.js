eaeaa686e7c4c18b5eb91c15bee8872f60acc204
IvaZach <ivazacherniuk@gmail.com>
2024-02-21 12:21:02 +0300

    add file sendEmail.js

---
+++ helpers/sendEmail.js
@@ -0,0 +1,35 @@
+import ElasticEmail from "@elasticemail/elasticemail-client";
+import "dotenv/config";
+
+const { ELASTICEMAIL_API_KEY, ELASTICEMAIL_EMAIL_FROM } = process.env;
+
+const defaultClient = ElasticEmail.ApiClient.instance;
+
+const { apikey } = defaultClient.authentications;
+apikey.apiKey = ELASTICEMAIL_API_KEY;
+
+const api = new ElasticEmail.EmailsApi();
+
+const email = ElasticEmail.EmailMessageData.constructFromObject({
+  Recipients: [new ElasticEmail.EmailRecipient("xogar78146@gexige.com ")],
+  Content: {
+    Body: [
+      ElasticEmail.BodyPart.constructFromObject({
+        ContentType: "HTML",
+        Content: "<strong>Test email</strong>",
+      }),
+    ],
+    Subject: "Subj:Test email",
+    From: ELASTICEMAIL_EMAIL_FROM,
+  },
+});
+
+const callback = function (error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("API called successfully.");
+  }
+};
+
+api.emailsPost(email, callback);

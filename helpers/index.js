ebf93913d1ae3fb4b1ebcef3a68feb1072f3cd1b
IvaZach <ivazacherniuk@gmail.com>
2024-02-21 14:38:55 +0300

    add verify

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,4 @@
 export { default as HttpError } from "./HttpError.js";
 export { default as validateBody } from "../decorators/validateBody.js";
 export {default as notFoundResult} from './notFoundResult.js'
+export {default as sendEmail} from './sendEmail.js'
--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 const { ELASTICEMAIL_SMTP_KEY, ELASTICEMAIL_EMAIL_FROM } = process.env;
 
 const nodemailerConfig = {
-  host: "smtp25.elasticemail.com",
+  host: "smtp.elasticemail.com",
   port: 2525,
   secure: false,
   auth: {
@@ -15,14 +15,10 @@ const nodemailerConfig = {
 
 const transport = nodemailer.createTransport(nodemailerConfig);
 
-const email = {
-  from: ELASTICEMAIL_EMAIL_FROM,
-  to: "xogar78146@gexige.com",
-  subject: "Subj:test email",
-  html: "<strong>Test email</strong>",
-};
 
-transport
-  .sendMail(email)
-  .then(() => console.log("Email send success"))
-  .catch((error) => console.error(error.message));
+const sendEmail = data => {
+    const email = {...data, from: ELASTICEMAIL_EMAIL_FROM};
+    return transport.sendMail(email)
+}
+
+export default sendEmail;
